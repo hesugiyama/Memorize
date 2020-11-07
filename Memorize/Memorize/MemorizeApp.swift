7d86d0bd32bfcdfd78069cad8292209e4d6c8724
Henrique Sugiyama <hsugiyama@ymail.com>
2020-11-07 11:16:11 -0300

    Added Xcode environment with basics of SwiftUI's

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Henrique on 04/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
